b6e44a5f62d59ced86876826698becf31b0b7b23
Glitch (glitch-hello-node) <none>
2021-08-15 02:43:20 +0000

    🐌🌵 Checkpoint ./server.js:34833427/346

--- server.js
+++ server.js
@@ -77,3 +77,14 @@ function checkAuth(req, res, next) {
 http.listen(port, () => {
   console.log(`[system] running`);
 });
+
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * 
+ charactersLength));
+   }
+   return result;
+}
