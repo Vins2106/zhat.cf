3bba3b81c650c031662e3f21bd5a65b684f6359d
Glitch (glitch-hello-node) <none>
2021-08-15 01:57:08 +0000

    🦂🍬 Checkpoint ./server.js:34833427/557 ./views/login.ejs:34833427/103 ./package.json:34833427/816

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
+const session = require("express-session");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use(session({
+  secret: 'super cat',
+  resave: false,
+  saveUninitialized: true
+}));
 
 app.use(express.static(__dirname + "/public"));
 
@@ -13,17 +24,23 @@ app.get('/', checkAuth, (req, res) => {
 
 app.get('/login', async (req, res) => {
   res.render("login.ejs")
-})
+});
+
+app.post('/login', async (req, res) => {
+  console.log(req.body);
+  
+  res.redirect("/login")
+});
 
 app.use("/", async (req, res) => {
   res.status(404).render("404.ejs")
-})
+});
 
 io.on('connection', (socket) => {
 });
 
 function checkAuth(req, res, next) {
-  if (req.session) return next();
+  if (req.session.user) return next();
   
   return res.redirect("/login")
 }
