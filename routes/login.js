340ab1b179ac5c10782f9c3c299d538bca483ed1
Glitch (glitch-hello-node) <none>
2021-08-18 06:53:29 +0000

    👟💅 Checkpoint ./server.js:34833427/924 ./routes/login.js:34833427/2558

---
+++ routes/login.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express.Routes;
+let data = require("../mongo/data.js");
+
+app.get('/', async (req, res) => {
+  if (req.query.error == "true") {
+    return res.render("login.ejs", {
+      error: true,
+      msg: req.query.message
+    })
+  }
+  
+  res.render("login.ejs", {
+    error: false
+  })
+});
+
+app.post('/', async (req, res) => {
+  
+  let checkFirstEmail = await data.findOne({Email: req.body.email});
+  if (!checkFirstEmail) return res.redirect("/login?error=true&message=Invalid email");
+  
+  let checkFirstEmailPassword = await data.findOne({Email: req.body.email, Password: req.body.password});
+  if (!checkFirstEmailPassword) return res.redirect("/login?error=true&message=Invalid email & password");
+  
+  req.session.user = checkFirstEmailPassword;
+  
+  if (req.query.callback) {
+    return res.redirect(req.query.callback);
+  }
+  
+  res.redirect("/")
+});
--- server.js
+++ server.js
@@ -82,35 +82,9 @@ app.get('/', checkAuth, async (req, res) => {
   }, 500)
 });
 
-app.get('/login', async (req, res) => {
-  if (req.query.error == "true") {
-    return res.render("login.ejs", {
-      error: true,
-      msg: req.query.message
-    })
-  }
-  
-  res.render("login.ejs", {
-    error: false
-  })
-});
+const loginRoutes = require("./routes/login.js");
 
-app.post('/login', async (req, res) => {
-  
-  let checkFirstEmail = await data.findOne({Email: req.body.email});
-  if (!checkFirstEmail) return res.redirect("/login?error=true&message=Invalid email");
-  
-  let checkFirstEmailPassword = await data.findOne({Email: req.body.email, Password: req.body.password});
-  if (!checkFirstEmailPassword) return res.redirect("/login?error=true&message=Invalid email & password");
-  
-  req.session.user = checkFirstEmailPassword;
-  
-  if (req.query.callback) {
-    return res.redirect(req.query.callback);
-  }
-  
-  res.redirect("/")
-});
+app.use("/login", loginRoutes)
 
 app.get("/signup", async (req, res) => {
   if (req.query.error == "true") {
