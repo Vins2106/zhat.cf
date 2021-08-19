e155428b1920c7e38b90f97400bab696978b15c7
Glitch (glitch-hello-node) <none>
2021-08-19 15:44:04 +0000

    🏟⛑ Checkpoint ./public/sw.js:34833427/483 ./views/parts/betahead.ejs:34833427/1774 ./public/manifest.json:34833427/34

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,3 @@
-if ('serviceWorker' in navigator && 'PushManager' in window) {
-  console.log('Service Worker and Push is supported');
-
-  navigator.serviceWorker.register('sw.js')
-  .then(function(swReg) {
-    console.log('Service Worker is registered', swReg);
-  })
-  .catch(function(error) {
-    console.error('Service Worker Error', error);
-  });
-} else {
-  console.warn('Push messaging is not supported');
-}
+self.addEventListener("fetch", function(event) {
+  console.log(`start server worker`)
+});
