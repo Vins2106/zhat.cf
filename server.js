3d25ddd30c07c9bea9e7d7b12ee2ab803235c4cd
Glitch (glitch-hello-node) <none>
2021-08-26 06:07:10 +0000

    🏨🏋 Checkpoint ./server.js:34833427/500

--- server.js
+++ server.js
@@ -25,7 +25,24 @@ const fetch = require("node-fetch")
 
 //
 const WebSocket = require("ws")
-const ws = new WebSocket.Server({server: http})
+const wss = new WebSocket.Server(http)
+
+
+wss.on('connection', function connection(ws) {
+  console.log('A new client Connected!');
+  ws.send('Welcome New Client!');
+
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+    
+  });
+});
 
 require("dotenv").config()
 require("./bot.js")()
