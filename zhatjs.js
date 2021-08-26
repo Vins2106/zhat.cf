31b1a04658123ac54f644dea6a1539cc51067eab
Glitch (glitch-hello-node) <none>
2021-08-26 09:00:08 +0000

    🚆🗻 Checkpoint ./zhatjs.js:34833427/878

--- zhatjs.js
+++ zhatjs.js
@@ -1,24 +1,3 @@
 module.exports = () => {
-const WebSocket = require("ws");
-
-
-const ws = new WebSocket('wss://zhat.cf/api/bot/v1/login?token=')
-
-ws.on('open', function open() {
-  console.log('connected');
-  ws.send(Date.now());
-});
-
-ws.on('close', function close() {
-  console.log('disconnected');
-});
-
-ws.on('message', function incoming(data) {
-  console.log(`Roundtrip time: ${Date.now() - data} ms`);
-
-  setTimeout(function timeout() {
-    ws.send(Date.now());
-  }, 500);
-});
-
+  
 }
