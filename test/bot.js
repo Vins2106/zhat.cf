6553f8ac6fd46c348ca5a7c5113057deb13118a6
Glitch (glitch-hello-node) <none>
2021-08-27 09:57:39 +0000

    ⛰💐 Checkpoint ./test/bot.js:34833427/37 ./package.json:34833427/1598

--- test/bot.js
+++ test/bot.js
@@ -18,7 +18,6 @@ module.exports = () => {
     }
     
     if (cmd == "ping") {
-      console.log("A")
       let timeNow = Date.now();
       return message.author.send(`ping: ${timeNow - message.createAt}ms`)
     }
