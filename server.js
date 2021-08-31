86ab65aff11c502cf8119c3bd1f645d53c191291
Glitch (glitch-hello-node) <none>
2021-08-31 15:19:22 +0000

    🎈👗 Checkpoint ./views/vc1.ejs:34833427/723 ./server.js:34833427/276

--- server.js
+++ server.js
@@ -264,6 +264,15 @@ let ison = {};
 let cds = {};
 
 io.on("connection", socket => {
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId)
+    socket.to(roomId).emit('user-connected', userId)
+
+    socket.on('disconnect', () => {
+      socket.to(roomId).emit('user-disconnected', userId)
+    })
+  })  
+  
   socket.on("updateOnline", userid => {
     io.sockets.emit("someoneOnline", userid);
   });
